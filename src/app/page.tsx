bd1c4051da53c067fd2badbe61e772aa434ca659
unknown <info.goldassessoria@gmail.com>
2026-04-24 00:47:04 -0300

    refactor: update call to action description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default async function Home({
               ✅ Sua análise da pizzaria foi liberada!
             </CardTitle>
             <CardDescription className="text-sm sm:text-xl font-medium text-muted-foreground">
-              Agora falta só um passo para receber seu diagnóstico completo:
+              Preencha as informações abaixo para receber uma análise completa da sua PIZZARIA e descobrir como aumentar suas vendas.
             </CardDescription>
           </CardHeader>
           
